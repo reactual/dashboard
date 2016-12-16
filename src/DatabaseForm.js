e0c980b09bd22630aeaff84a82e974229a9dddbb
Chris Anderson <chris@fauna.com>
2016-12-15 16:15:22 -0800

    refactor database form

--- src/DatabaseForm.js
+++ src/DatabaseForm.js
@@ -5,6 +5,45 @@ import clientForSubDB from "./clientForSubDB";
 import faunadb from 'faunadb';
 const q = faunadb.query, Ref = q.Ref;
 
+class SchemaForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {disabled:false}
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit(event) {
+    event.preventDefault()
+    this.setState({disabled:true});
+    this.props.onSubmit().then(() => {
+      this.setState({disabled:false});
+      this.props.bumpSchema();
+    }, () => {
+      this.setState({disabled:false});
+    })
+  }
+  render() {
+    return (
+      <div className="SchemaForm">
+        <form>
+          {this.props.children}
+          <Button disabled={!!this.state.disabled}
+            buttonType={ ButtonType.primary } onClick={this.onSubmit}>{this.props.buttonText}</Button>
+        </form>
+      </div>
+    )
+  }
+}
+
+function clientForPath(client, path, type) {
+  if (!client) return;
+  if (path) {
+    return clientForSubDB(client, path, type);
+  } else {
+    // we are in a server key context so we don't know our path and can't change our client
+    return client;
+  }
+}
+
 export class DatabaseForm extends Component {
   constructor(props) {
     super(props)
@@ -12,25 +51,12 @@ export class DatabaseForm extends Component {
     this.onSubmit = this.onSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
   }
-  onSubmit(event) {
-    event.preventDefault();
-    var path = this.props.params.splat;
-    var client = this.props.client;
-    if (!client) return;
-    var scopedClient;
-    if (path) {
-      scopedClient = clientForSubDB(client, path, "admin");
-    } else {
-      // we are in a server key context
-      // so we don't know our path and can't change our client
-      scopedClient = client;
-    }
-    this.setState({creating:true});
-    scopedClient.query(q.Create(Ref("databases"), { name: this.state.form.name })).then( (res) => {
-      console.log("created",res);
-      this.setState({creating:false, form:{name:""}});
-      this.props.bumpSchema();
-    })
+  onSubmit() {
+    return clientForPath(this.props.client, this.props.params.splat, "admin")
+      .query(q.Create(Ref("databases"), { name: this.state.form.name }))
+      .then((res)=>{
+        this.setState({form:{name:""}});
+      })
   }
   onChange(field, value) {
     var form = this.state.form;
@@ -41,17 +67,14 @@ export class DatabaseForm extends Component {
     var context = this.props.params.splat ? " in "+this.props.params.splat : "";
     const form = this.state.form;
     return (
-      <div className="DatabaseForm">
-        <form>
-          <h3>Create a database{context}</h3>
-          <TextField label="Name"
-            required={true}
-            description="This name is used in queries and API calls."
-            value={form.name}
-            onChanged={this.onChange.bind(this, "name")}/>
-          <Button disabled={!!this.state.creating} buttonType={ ButtonType.primary } onClick={this.onSubmit}>Create Database</Button>
-        </form>
-      </div>
+      <SchemaForm buttonText="Create Databases" onSubmit={this.onSubmit} bumpSchema={this.props.bumpSchema}>
+        <h3>Create a database{context}</h3>
+        <TextField label="Name"
+          required={true}
+          description="This name is used in queries and API calls."
+          value={form.name}
+          onChanged={this.onChange.bind(this, "name")}/>
+      </SchemaForm>
     )
   }
 }
