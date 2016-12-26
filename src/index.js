ceec2e0bf3731523f8cb6c813d954e3a3090c6ab
Marrony Neris <marrony@fauna.com>
2016-12-26 15:59:36 -0200

    add redux to project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
 import App from './App';
 import './index.css';
 
+function appReducer(state, action) {
+  return state;
+}
+
+let store = createStore(appReducer)
+
+store.subscribe(() => {
+  console.log(store.getState())
+});
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
