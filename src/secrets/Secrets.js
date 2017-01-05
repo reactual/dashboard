7a65ff21a3b3ba659eab86f326d535a39387841f
Erick Pintor <erickpintor@gmail.com>
2017-01-05 13:40:52 -0200

    make secret form submit on enter key

--- src/secrets/Secrets.js
+++ src/secrets/Secrets.js
@@ -18,34 +18,34 @@ export class SecretForm extends Component {
     setter[key] = value;
     this.setState(setter);
   }
-  handleSubmit() {
-    this.props.onSubmit(this.state)
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
   }
   render() {
     return (
       <div className="SecretForm">
+          <Dialog
+            isOpen={ this.props.showDialog }
+            type={ DialogType.largeHeader }
+            title='Connect to FaunaDB'
+            subText='Visit https://fauna.com/account/keys or talk to your administrator to provision keys.'
+            isBlocking={ true }
+          >
+            <form>
+              <TextField label="FaunaDB Endpoint URL"
+                description="Leave this empty for Fauna Cloud."
+                placeholder="https://db.fauna.com/"
+                value={this.state.endpoint} onChanged={this.handleChange.bind(this,"endpoint")}/>
+              <TextField required type="password" label="Key Secret"
+                description="The secret associated with your FaunaDB key."
+                value={this.state.secret} onChanged={this.handleChange.bind(this,"secret")}/>
 
-        <Dialog
-          isOpen={ this.props.showDialog }
-          type={ DialogType.largeHeader }
-          title='Connect to FaunaDB'
-          subText='Visit https://fauna.com/account/keys or talk to your administrator to provision keys.'
-          isBlocking={ true }
-        >
-        <TextField label="FaunaDB Endpoint URL"
-          description="Leave this empty for Fauna Cloud."
-          placeholder="https://db.fauna.com/"
-          value={this.state.endpoint} onChanged={this.handleChange.bind(this,"endpoint")}/>
-        <TextField required type="password" label="Key Secret"
-          description="The secret associated with your FaunaDB key."
-          value={this.state.secret} onChanged={this.handleChange.bind(this,"secret")}/>
-
-          <DialogFooter>
-            <Button buttonType={ ButtonType.primary } onClick={this.handleSubmit}>Use Secret</Button>
-          </DialogFooter>
-        </Dialog>
-
-
+                <DialogFooter>
+                  <Button buttonType={ ButtonType.primary } onClick={this.handleSubmit}>Use Secret</Button>
+                </DialogFooter>
+            </form>
+          </Dialog>
       </div>
     )
   }
