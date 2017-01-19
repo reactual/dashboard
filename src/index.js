67eba295994ff6fcc737c0a088075d6d6473b491
Marrony Neris <marrony@fauna.com>
2017-01-19 13:29:41 -0200

    logs actions instead of state changes (only in dev mode)

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
 import { App, appReducer } from './app'
 import './index.css';
 
+var middlewares = [thunk]
+
+if(process.env.NODE_ENV === 'development') {
+  middlewares = [...middlewares, createLogger({collapsed: true})]
+}
+
 const store = createStore(
   appReducer,
-  applyMiddleware(thunk)
+  applyMiddleware(...middlewares)
 )
 
-store.subscribe(() => {
-  console.log("redux state", store.getState())
-});
 
 ReactDOM.render(
   <App store={store}/>,
