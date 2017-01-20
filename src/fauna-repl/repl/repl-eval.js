ee156fcc342862540f5e1f36fe9f5302961dd0c9
Erick Pintor <erickpintor@gmail.com>
2017-01-20 19:06:32 -0200

    wrapping errors during repl evaluation

--- src/fauna-repl/repl/repl-eval.js
+++ src/fauna-repl/repl/repl-eval.js
@@ -3,6 +3,10 @@ import { query as q } from 'faunadb'
 import { queryFunctionsAsGlobalVariables } from './query-functions'
 
 export default function replEval(client, __query) {
-  const query = eval(`(function() {${queryFunctionsAsGlobalVariables}; return ${__query};})()`)
-  return client.query(query)
+  try {
+    const query = eval(`(function() {${queryFunctionsAsGlobalVariables}; return ${__query};})()`)
+    return client.query(query)
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }
