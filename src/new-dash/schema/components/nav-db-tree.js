31e265dba585d409eb5e82dd731cb884f1fd2a32
Erick Pintor <erickpintor@gmail.com>
2017-02-02 13:48:23 -0200

    customizing nav tree layout

--- src/new-dash/schema/components/nav-db-tree.js
+++ src/new-dash/schema/components/nav-db-tree.js
@@ -2,12 +2,7 @@ import React from "react"
 import Immutable from "immutable"
 import { connect } from "react-redux"
 import { browserHistory } from "react-router"
-import { Nav } from 'office-ui-fabric-react';
-
-const onClick = (url) => (e) => {
-  e.preventDefault()
-  browserHistory.push(url)
-}
+import { Nav, css } from "office-ui-fabric-react";
 
 const databaseLinks = (schema, dbUrl = "") => {
   const databaseTree = (db) => {
@@ -19,7 +14,6 @@ const databaseLinks = (schema, dbUrl = "") => {
       url,
       key: url,
       isExpanded: true,
-      onClick: onClick(url),
       links: databaseLinks(db, url)
     }
   }
@@ -31,17 +25,40 @@ const databaseLinks = (schema, dbUrl = "") => {
     .toJS()
 }
 
+const onClick = (e, link) => {
+  e.preventDefault()
+  browserHistory.push(link.url)
+}
+
 const NavDBTree = (props) => {
   const links = [{
-    links: [{
-      name: "Databases",
-      isExpanded: true,
-      onClick: onClick("/"),
-      links: databaseLinks(props.schema)
-    }]
+    name: "Databases",
+    isExpanded: true,
+    links: databaseLinks(props.schema)
   }]
 
-  return <Nav groups={links} />
+  return <Flav groups={links} onLinkClick={onClick} />
+}
+
+// Custom nav tree to add chevron icon to all sub databases
+class Flav extends Nav {
+  _renderCompositeLink(link, linkIndex, nestingLevel) {
+    const key = link.key || linkIndex
+    const isLinkSelected = key === this.state.selectedKey
+
+    return <div key={key}
+        className={css("ms-Nav-compositeLink", {"is-expanded": link.isExpanded, "is-selected": isLinkSelected })}>
+          {(link.links && link.links.length > 0 ?
+            <button
+              className="ms-Nav-chevronButton ms-Nav-chevronButton--link"
+              onClick={this._onLinkExpandClicked.bind(this, link)}
+              title={(link.isExpanded ? this.props.expandedStateText : this.props.collapsedStateText)}>
+              <i className="ms-Nav-chevron ms-Icon ms-Icon--ChevronDown"></i>
+            </button> : null
+          )}
+        {!!link.onClick ? this._renderButtonLink(link, linkIndex) : this._renderAnchorLink(link, linkIndex, nestingLevel)}
+      </div>
+  }
 }
 
 export default connect(
--- src/new-dash/schema/components/nav-tree.js
+++ src/new-dash/schema/components/nav-tree.js
@@ -6,10 +6,10 @@ import NavSchema from "./nav-schema.js"
 
 const NavTree = (props) => {
   return <div className="ms-Grid-row">
-      <div className="ms-Grid-col ms-u-sm12 ms-u-md6 ms-u-lg6">
+      <div className="ms-Grid-col ms-u-sm12 ms-u-xl6">
         <NavDBTree />
       </div>
-      <div className="ms-Grid-col ms-u-sm12 ms-u-md6 ms-u-lg6">
+      <div className="ms-Grid-col ms-u-sm12 ms-u-xl6">
         <NavSchema />
       </div>
     </div>
