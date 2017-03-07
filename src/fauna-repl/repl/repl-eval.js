6632dfa08d3d2a127798f6a38ba35dcb9f0076c0
Erick Pintor <erickpintor@gmail.com>
2017-03-07 16:13:41 -0300

    removing legacy code

--- src/fauna-repl/repl/repl-eval.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-export default function replEval(q, client, __query) {
-  try {
-    const query = eval(__query)
-    return client.query(query)
-  } catch (error) {
-    return Promise.reject(error)
-  }
-}
