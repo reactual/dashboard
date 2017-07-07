3f360a784ebcc2423ff231010302b1d8bf5a2198
Erick Pintor <erickpintor@gmail.com>
2017-07-07 16:44:20 -0300

    move sentry out of base dashboard

--- packages/dashboard-base/src/index.js
+++ packages/dashboard-base/src/index.js
@@ -1,15 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Raven from "raven-js"
 
 import Dashboard from "./dashboard"
-
-// FIXME: sentry should be at a plugin and enabled for could only
-if (process.env.NODE_ENV === "production") {
-  Raven
-    .config("https://a6a14ab8e3ab4cbb87edaa320ad57ecb@sentry.io/154810")
-    .install()
-}
 
 ReactDOM.render(
   <Dashboard />,
--- packages/dashboard-cloud/src/index.js
+++ packages/dashboard-cloud/src/index.js
@@ -1,13 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import ReactGA from "react-ga"
+import Raven from "raven-js"
 import { Dashboard } from "dashboard-base"
 
-require("./ga-events")
+const isProduction = process.env.NODE_ENV === "production"
+
+if (isProduction) {
+  Raven.config("https://a6a14ab8e3ab4cbb87edaa320ad57ecb@sentry.io/154810").install()
+  require("./ga-events")
+}
 
 class CloudDashboard extends React.Component {
   componentDidMount() {
-    if (process.env.NODE_ENV === "production") {
+    if (isProduction) {
       ReactGA.initialize("UA-51914115-2")
     }
   }
